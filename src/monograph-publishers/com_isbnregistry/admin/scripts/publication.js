4143a2bd70a9496194c7d38894991bb241709f3d
Petteri Kivimäki <dinky_jackson@hotmail.com>
2015-12-05 17:56:57 +0200

    Added Chosen to publisher backend forms.

--- src/monograph-publishers/com_isbnregistry/admin/scripts/publication.js
+++ src/monograph-publishers/com_isbnregistry/admin/scripts/publication.js
@@ -9,4 +9,8 @@ jQuery(document).ready(function ($) {
         width: "17em",
         placeholder_text_multiple: " "
     });	
+	$("#jform_publisher_id, #jform_lang_code, #jform_publication_type, #jform_publication_format, #jform_language, #jform_year, #jform_month,#jform_edition").chosen({
+		disable_search_threshold: 10,
+		width: "17em"
+	});
 });
