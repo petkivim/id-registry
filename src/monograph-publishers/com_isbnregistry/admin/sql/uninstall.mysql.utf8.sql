af9f07ffd3fa30c2ee01485948a35493893d431f
Petteri Kivimäki <dinky_jackson@hotmail.com>
2015-12-05 10:50:18 +0200

    Updated DB uninstall file.

--- src/monograph-publishers/com_isbnregistry/admin/sql/uninstall.mysql.utf8.sql
+++ src/monograph-publishers/com_isbnregistry/admin/sql/uninstall.mysql.utf8.sql
@@ -1,2 +1,5 @@
 DROP TABLE IF EXISTS `#__isbn_registry_publisher`;
 DROP TABLE IF EXISTS `#__isbn_registry_publication`;
+DROP TABLE IF EXISTS `#__isbn_registry_isbn_range`;
+DROP TABLE IF EXISTS `#__isbn_registry_publisher_isbn_range`;
+DROP TABLE IF EXISTS `#__isbn_registry_isbn_used`;
