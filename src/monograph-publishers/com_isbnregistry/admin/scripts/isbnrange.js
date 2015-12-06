65c8461ae4d96d52b9e0aa2b31a32e70d100445e
Petteri Kivimäki <dinky_jackson@hotmail.com>
2015-12-06 07:53:33 +0200

    Added Chosen to Publisher and ISBN Range.
    Publisher and isbn range bakcend forms are using Chosen.

---
+++ src/monograph-publishers/com_isbnregistry/admin/scripts/isbnrange.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function ($) {
+	$("#jform_prefix, #jform_lang_group").chosen({
+		disable_search_threshold: 10,
+		width: "17em"
+	});	
+});
--- src/monograph-publishers/com_isbnregistry/admin/scripts/publisher.js
+++ src/monograph-publishers/com_isbnregistry/admin/scripts/publisher.js
@@ -4,4 +4,8 @@ jQuery(document).ready(function ($) {
         width: "17em",
         placeholder_text_multiple: " "
     });
+	$("#jform_lang_code").chosen({
+		disable_search_threshold: 10,
+		width: "17em"
+	});	
 });
