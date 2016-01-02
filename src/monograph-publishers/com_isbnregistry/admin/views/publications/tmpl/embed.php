44a38b799f8ef444fc23ca725a5576746b2691c3
Petteri Kivimäki <dinky_jackson@hotmail.com>
2016-01-02 17:27:45 +0200

    Added form element to embed publications
    Put publications on edit publisher view inside a form element. Thanks to
    that pagination is now working in embedded publications list too.

--- src/monograph-publishers/com_isbnregistry/admin/views/publications/tmpl/embed.php
+++ src/monograph-publishers/com_isbnregistry/admin/views/publications/tmpl/embed.php
@@ -9,80 +9,81 @@
 defined('_JEXEC') or die('Restricted Access');
 JHTML::_('behavior.modal');
 ?>
-<table class="table table-striped table-hover">
-    <thead>
-        <tr>
-            <th width="1%"><?php echo JText::_('COM_ISBNREGISTRY_PUBLICATIONS_NUM'); ?></th>
-            <th width="20%">
-                <?php echo JText::_('COM_ISBNREGISTRY_PUBLICATIONS_TITLE'); ?>
-            </th>	
-            <th width="15%">
-                <?php echo JText::_('COM_ISBNREGISTRY_PUBLICATIONS_IDENTIFIER'); ?>
-            </th>
-            <th width="30%">
-                <?php echo JText::_('COM_ISBNREGISTRY_PUBLICATIONS_COMMENTS'); ?>
-            </th>		
-            <th width="15%">
-                <?php echo JText::_('COM_ISBNREGISTRY_PUBLICATIONS_CREATED'); ?>
-            </th>	
-            <th width="15%">
-                <?php echo JText::_('COM_ISBNREGISTRY_PUBLICATIONS_MODIFIED'); ?>
-            </th>
-            <th width="2%">
-                <?php echo JText::_('COM_ISBNREGISTRY_PUBLICATIONS_ID'); ?>
-            </th>
-        </tr>
-    </thead>
-    <tfoot>
-        <tr>
-            <td colspan="5">
-                <?php echo $this->pagination->getListFooter(); ?>
-            </td>
-        </tr>
-    </tfoot>
-    <tbody>
-        <?php if (!empty($this->items)) : ?>
-            <?php
-            foreach ($this->items as $i => $row) :
-                $link = JRoute::_('index.php?option=com_isbnregistry&task=publication.edit&id=' . $row->id);
-                ?>
-                <tr>
-                    <td><?php echo $this->pagination->getRowOffset($i); ?></td>   
-                    <td>
-                        <a target="new" href="<?php echo $link; ?>" title="<?php echo JText::_('COM_ISBNREGISTRY_EDIT_PUBLICATION'); ?>">
-                            <?php echo $row->title; ?>
-                        </a>
-                    </td> 
-                    <td>
-                        <?php echo $row->publication_identifier; ?>
-                    </td> 
-                    <td>
-                        <?php echo (strlen($row->comments) > 50 ? substr($row->comments, 0, 47) . '...' : $row->comments); ?>
-                    </td>   
-                    <td>
-                        <?php echo JHtml::date($row->created, 'd.m.Y'); ?>
-                    </td>
-                    <td>
-                        <?php echo strcmp($row->modified, '0000-00-00 00:00:00') == 0 ? '-' : JHtml::date($row->modified, 'd.m.Y'); ?>
-                    </td> 
-                    <td align="center">
-                        <?php echo $row->id; ?>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-        <?php else : ?>
+<form action="index.php?option=com_isbnregistry&view=publications&publisherId=<?php echo $this->item->id; ?>&tmpl=component&layout=embed" method="post" id="adminForm" name="adminForm">
+    <table class="table table-striped table-hover">
+        <thead>
             <tr>
-                <td></td>
-                <td>
-                    <?php echo JText::_('COM_ISBNREGISTRY_PUBLICATIONS_NONE'); ?>
-                </td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <th width="1%"><?php echo JText::_('COM_ISBNREGISTRY_PUBLICATIONS_NUM'); ?></th>
+                <th width="20%">
+                    <?php echo JText::_('COM_ISBNREGISTRY_PUBLICATIONS_TITLE'); ?>
+                </th>	
+                <th width="15%">
+                    <?php echo JText::_('COM_ISBNREGISTRY_PUBLICATIONS_IDENTIFIER'); ?>
+                </th>
+                <th width="30%">
+                    <?php echo JText::_('COM_ISBNREGISTRY_PUBLICATIONS_COMMENTS'); ?>
+                </th>		
+                <th width="15%">
+                    <?php echo JText::_('COM_ISBNREGISTRY_PUBLICATIONS_CREATED'); ?>
+                </th>	
+                <th width="15%">
+                    <?php echo JText::_('COM_ISBNREGISTRY_PUBLICATIONS_MODIFIED'); ?>
+                </th>
+                <th width="2%">
+                    <?php echo JText::_('COM_ISBNREGISTRY_PUBLICATIONS_ID'); ?>
+                </th>
             </tr>
-        <?php endif; ?>
-    </tbody>
-</table>
-
+        </thead>
+        <tfoot>
+            <tr>
+                <td colspan="5">
+                    <?php echo $this->pagination->getListFooter(); ?>
+                </td>
+            </tr>
+        </tfoot>
+        <tbody>
+            <?php if (!empty($this->items)) : ?>
+                <?php
+                foreach ($this->items as $i => $row) :
+                    $link = JRoute::_('index.php?option=com_isbnregistry&task=publication.edit&id=' . $row->id);
+                    ?>
+                    <tr>
+                        <td><?php echo $this->pagination->getRowOffset($i); ?></td>   
+                        <td>
+                            <a target="new" href="<?php echo $link; ?>" title="<?php echo JText::_('COM_ISBNREGISTRY_EDIT_PUBLICATION'); ?>">
+                                <?php echo $row->title; ?>
+                            </a>
+                        </td> 
+                        <td>
+                            <?php echo $row->publication_identifier; ?>
+                        </td> 
+                        <td>
+                            <?php echo (strlen($row->comments) > 50 ? substr($row->comments, 0, 47) . '...' : $row->comments); ?>
+                        </td>   
+                        <td>
+                            <?php echo JHtml::date($row->created, 'd.m.Y'); ?>
+                        </td>
+                        <td>
+                            <?php echo strcmp($row->modified, '0000-00-00 00:00:00') == 0 ? '-' : JHtml::date($row->modified, 'd.m.Y'); ?>
+                        </td> 
+                        <td align="center">
+                            <?php echo $row->id; ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else : ?>
+                <tr>
+                    <td></td>
+                    <td>
+                        <?php echo JText::_('COM_ISBNREGISTRY_PUBLICATIONS_NONE'); ?>
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            <?php endif; ?>
+        </tbody>
+    </table>
+</form>
