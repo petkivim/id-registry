a4ae5fcedf8c444777d708118b21f2c56a691de8
Petteri Kivimäki <dinky_jackson@hotmail.com>
2016-01-07 07:56:04 +0200

    Added readonly to button field.
    It's possible to define custom button field as readonly.

--- src/monograph-publishers/com_isbnregistry/admin/models/fields/button.php
+++ src/monograph-publishers/com_isbnregistry/admin/models/fields/button.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package     Joomla.Administrator
  * @subpackage  com_isbnregistry
@@ -6,20 +7,21 @@
  * @copyright	Copyright (C) 2015 Petteri Kivimäki. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die('Restricted access');
- 
+
 jimport('joomla.form.formfield');
- 
+
 class JFormFieldButton extends JFormField {
- 
-	protected $type = 'Button';
- 
-	// getLabel() left out
- 
-	public function getInput() {			   
-		$html .= '<input type="button" name="'.$this->name.'" id="'.$this->id.'" class="'.$this->class.'" value="'.JText::_($this->default).'" />';
-		return $html;
-	}
+
+    protected $type = 'Button';
+
+    // getLabel() left out
+
+    public function getInput() {
+        $readonly = $this->readonly ? ' disabled' : '';
+        $html .= '<input type="button" name="' . $this->name . '" id="' . $this->id . '" class="' . $this->class . '" value="' . JText::_($this->default) . '"' . $readonly . ' />';
+        return $html;
+    }
+
 }
