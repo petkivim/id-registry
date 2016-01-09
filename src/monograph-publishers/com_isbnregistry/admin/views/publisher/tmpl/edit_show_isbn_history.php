53df6119232749af49a6a172d109805c4565b574
Petteri Kivimäki <dinky_jackson@hotmail.com>
2016-01-09 14:45:09 +0200

    Bug fix.
    When creating new publisher the identifier history showed all the
    identifiers in the system. Fixed.

--- src/monograph-publishers/com_isbnregistry/admin/views/publisher/tmpl/edit_show_isbn_history.php
+++ src/monograph-publishers/com_isbnregistry/admin/views/publisher/tmpl/edit_show_isbn_history.php
@@ -10,7 +10,8 @@
 // No direct access
 defined('_JEXEC') or die('Restricted access');
 
-$link = '<a href="' . JRoute::_('index.php?option=com_isbnregistry&view=identifierbatches&tmpl=component&layout=embed&type=isbn&publisherId=' . $this->item->id . '');
+
+$link = '<a href="' . JRoute::_('index.php?option=com_isbnregistry&view=identifierbatches&tmpl=component&layout=embed&type=isbn&publisherId=' . ($this->item->id > 0 ? $this->item->id : -1));
 $link .= '" class="modal" rel="{size: {x: 1200, y: 600}, handler:\'iframe\'}">' . JText::_('COM_ISBNREGISTRY_PUBLISHER_FIELD_SHOW_IDENTIFIER_HISTORY') . '</a>';
 echo $link;
 ?>
