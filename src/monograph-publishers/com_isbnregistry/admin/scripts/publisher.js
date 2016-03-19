33663c1eda1a9347eb0fee2f3ac783a9e70cc902
Petteri Kivimäki <dinky_jackson@hotmail.com>
2016-03-19 08:38:59 +0200

    ISBN range info on edit publisher updated
    Next column was changed to taken.

--- src/monograph-publishers/com_isbnregistry/admin/scripts/publisher.js
+++ src/monograph-publishers/com_isbnregistry/admin/scripts/publisher.js
@@ -169,7 +169,7 @@ jQuery(document).ready(function ($) {
                             content += '<td class="' + type + '_range_col_1">' + data[i].publisher_identifier + '-' + padding + '-X' + '</td>';
                             content += '<td class="' + type + '_range_col_2">' + data[i].created.split(' ')[0] + ' (' + data[i].created_by + ')</td>';
                             content += '<td class="' + type + '_range_col_3">' + data[i].free + '</td>';
-                            content += '<td class="' + type + '_range_col_4">' + data[i].next + '</td>';
+                            content += '<td class="' + type + '_range_col_4">' + data[i].taken + '</td>';
                             content += '<td class="' + type + '_range_col_5';
                             if (data[i].is_active == 1) {
                                 content += '">' + label_active;
