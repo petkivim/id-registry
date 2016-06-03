60c5f220452c5f0678b423d2ab6076ad51ae8a08
Petteri Kivimäki <dinky_jackson@hotmail.com>
2016-06-03 20:34:31 +0300

    Added search contains true to ISSN publisher seach
    Added search contains true to ISSN publisher seach on edit form view.

--- src/serials-publishers/com_issnregistry/admin/scripts/form.js
+++ src/serials-publishers/com_issnregistry/admin/scripts/form.js
@@ -13,7 +13,8 @@ jQuery(document).ready(function ($) {
 
     $("#jform_lang_code, #jform_status, #jform_publisher_id").chosen({
         disable_search_threshold: 10,
-        width: "17em"
+        width: "17em",
+        search_contains: true
     });
 
     $("button[data-target='#modal-print']").click(function () {
